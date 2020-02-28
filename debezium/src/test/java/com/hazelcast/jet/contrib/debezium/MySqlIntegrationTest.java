dab02f0af2816ab9503c95ae45cf6773e91b917b
Emin Demirci <emin@hazelcast.com>
2020-02-28 16:22:00 +0300

    Update README and the test

--- debezium/src/test/java/com/hazelcast/jet/contrib/debezium/MySqlIntegrationTest.java
+++ debezium/src/test/java/com/hazelcast/jet/contrib/debezium/MySqlIntegrationTest.java
@@ -16,24 +16,33 @@
 
 package com.hazelcast.jet.contrib.debezium;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.hazelcast.jet.JetInstance;
 import com.hazelcast.jet.Job;
 import com.hazelcast.jet.config.JobConfig;
 import com.hazelcast.jet.core.JetTestSupport;
 import com.hazelcast.jet.core.JobStatus;
 import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.ServiceFactories;
 import com.hazelcast.jet.pipeline.test.AssertionCompletedException;
 import com.hazelcast.jet.pipeline.test.AssertionSinks;
 import io.debezium.config.Configuration;
+import io.debezium.serde.DebeziumSerdes;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.connect.data.Values;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.shaded.com.fasterxml.jackson.annotation.JsonIgnore;
+import org.testcontainers.shaded.com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Collections;
 import java.util.Objects;
 import java.util.concurrent.CompletionException;
 
@@ -62,14 +71,24 @@ public class MySqlIntegrationTest extends JetTestSupport {
                 .with("database.server.id", "184054")
                 .with("database.server.name", "dbserver1")
                 .with("database.whitelist", "inventory")
+                .with("table.whitelist", "inventory.customers")
+                .with("include.schema.changes", "false")
                 .with("database.history.hazelcast.list.name", "test")
                 .build();
-
 
         Pipeline pipeline = Pipeline.create();
         pipeline.readFrom(DebeziumSources.cdc(configuration))
                 .withoutTimestamps()
                 .map(record -> Values.convertToString(record.valueSchema(), record.value()))
+                .filterUsingService(ServiceFactories.sharedService(context -> {
+                    Serde<EventRecord> serde = DebeziumSerdes.payloadJson(EventRecord.class);
+                    serde.configure(Collections.emptyMap(), false);
+                    return serde.deserializer();
+                }, Deserializer::close), (deserializer, record) -> {
+                    EventRecord eventRecord = deserializer.deserialize("", record.getBytes());
+                    return eventRecord.isUpdate();
+                })
+                .peek()
                 .writeTo(AssertionSinks.assertCollectedEventually(30,
                         list -> Assert.assertTrue(list.stream().anyMatch(s -> s.contains("Anne Marie")))));
 
@@ -83,6 +102,7 @@ public class MySqlIntegrationTest extends JetTestSupport {
         Job job = jet.newJob(pipeline, jobConfig);
         assertJobStatusEventually(job, JobStatus.RUNNING);
 
+        sleepAtLeastSeconds(10);
         // update a record
         try (Connection connection = DriverManager.getConnection(mysql.withDatabaseName("inventory").getJdbcUrl(),
                 mysql.getUsername(), mysql.getPassword())) {
@@ -103,4 +123,39 @@ public class MySqlIntegrationTest extends JetTestSupport {
                     errorMsg.contains(AssertionCompletedException.class.getName()));
         }
     }
+
+    private static final class EventRecord implements Serializable {
+
+        @JsonIgnore
+        public JsonNode source;
+
+        @JsonIgnore
+        public JsonNode after;
+
+        @JsonIgnore
+        public JsonNode before;
+
+        @JsonIgnore
+        public long ts_ms;
+
+        @JsonProperty
+        public String op;
+
+        EventRecord() {
+        }
+
+        EventRecord(String operation) {
+            this.op = operation;
+        }
+
+        public boolean isUpdate() {
+            return "u".equals(op);
+        }
+
+        public boolean isCreate() {
+            return "c".equals(op);
+        }
+    }
+
+
 }
