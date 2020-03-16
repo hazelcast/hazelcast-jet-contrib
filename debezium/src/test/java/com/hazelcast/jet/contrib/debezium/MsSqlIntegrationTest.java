3ad0908e8ea4958332d59df19dd63bed4f7c3014
Bartok Jozsef <jozsi@hazelcast.com>
2020-03-16 14:58:57 +0200

    Fix the fix

--- debezium/src/test/java/com/hazelcast/jet/contrib/debezium/MsSqlIntegrationTest.java
+++ debezium/src/test/java/com/hazelcast/jet/contrib/debezium/MsSqlIntegrationTest.java
@@ -29,7 +29,7 @@ import org.apache.kafka.connect.data.Values;
 import org.junit.Rule;
 import org.junit.Test;
 import org.testcontainers.containers.BindMode;
-import org.testcontainers.containers.Container.ExecResult;
+import org.testcontainers.containers.Container;
 import org.testcontainers.containers.MSSQLServerContainer;
 
 import java.sql.Connection;
@@ -46,22 +46,17 @@ public class MsSqlIntegrationTest extends JetTestSupport {
 
     @Rule
     public MSSQLServerContainer mssql = new MSSQLServerContainer<>().withEnv("MSSQL_AGENT_ENABLED", "true")
-                                                                    .withClasspathResourceMapping("mssql/setup.sql",
-                                                                            "/tmp/setup.sql", BindMode.READ_ONLY)
-                                                                    .withClasspathResourceMapping("mssql/check.sql",
-                                                                            "/tmp/check.sql", BindMode.READ_ONLY);
+            .withClasspathResourceMapping("mssql/setup.sql",
+                    "/tmp/setup.sql", BindMode.READ_ONLY)
+            .withClasspathResourceMapping("mssql/cdc.sql",
+                    "/tmp/cdc.sql", BindMode.READ_ONLY);
 
     @Test
     public void readFromMsSql() throws Exception {
-        mssql.execInContainer("/opt/mssql-tools/bin/sqlcmd", "-S", "localhost", "-U", mssql.getUsername(),
-                "-P", mssql.getPassword(), "-d", "master", "-i", "/tmp/setup.sql");
-
+        execInContainer("setup.sql");
         assertTrueEventually(() -> {
-            ExecResult result = mssql.execInContainer("/opt/mssql-tools/bin/sqlcmd", "-S", "localhost", "-U",
-                    mssql.getUsername(),  "-P", mssql.getPassword(), "-d", "master", "-i", "/tmp/check.sql");
-            System.out.println("exec result: " + result);
-            assertContains(result.getStdout(), "MyDB");
-            assertContains(result.getStdout(), "Running");
+            Container.ExecResult result = execInContainer("cdc.sql");
+            assertContains(result.getStdout(), "already");
         });
 
         Configuration configuration = Configuration
@@ -117,5 +112,10 @@ public class MsSqlIntegrationTest extends JetTestSupport {
         }
 
 
+    }
+
+    private Container.ExecResult execInContainer(String script) throws Exception {
+        return mssql.execInContainer("/opt/mssql-tools/bin/sqlcmd", "-S", "localhost", "-U", mssql.getUsername(),
+                "-P", mssql.getPassword(), "-d", "master", "-i", "/tmp/" + script);
     }
 }
--- debezium/src/test/resources/mssql/check.sql
+++
@@ -1,9 +0,0 @@
-SELECT dss.[status], dss.[status_desc]
-FROM   sys.dm_server_services dss
-WHERE  dss.[servicename] LIKE N'SQL Server Agent (%';
-GO
-
-SELECT name, database_id, is_cdc_enabled FROM sys.databases
-WHERE is_cdc_enabled = 1 AND name = 'MyDB'
-GO
-
