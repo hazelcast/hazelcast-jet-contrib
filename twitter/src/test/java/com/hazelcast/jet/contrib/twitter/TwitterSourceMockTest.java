1b96648a7a6ec19e5312e456a21e2be3feb9f1e8
Emin Demirci <emin@hazelcast.com>
2020-03-20 12:33:14 +0300

    Refactor tests and split them into groups of nightly and regular

--- twitter/src/test/java/com/hazelcast/jet/contrib/twitter/TwitterSourceMockTest.java
+++ twitter/src/test/java/com/hazelcast/jet/contrib/twitter/TwitterSourceMockTest.java
@@ -17,7 +17,6 @@
 package com.hazelcast.jet.contrib.twitter;
 
 import com.hazelcast.internal.json.Json;
-import com.hazelcast.jet.JetInstance;
 import com.hazelcast.jet.Job;
 import com.hazelcast.jet.core.JetTestSupport;
 import com.hazelcast.jet.pipeline.BatchSource;
@@ -30,6 +29,7 @@ import com.hazelcast.jet.pipeline.test.AssertionSinks;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -52,59 +52,26 @@ public class TwitterSourceMockTest extends JetTestSupport {
     @Rule
     public final MockWebServer server = new MockWebServer();
 
-    private JetInstance jet;
-    private Properties credentials;
-
     @Before
-    public void setup() {
-        jet = createJetMember();
-        credentials = new Properties();
-        credentials.put("consumerKey", "mock_consumerKey");
-        credentials.put("consumerSecret", "mock_consumerSecret");
-        credentials.put("token", "mock_token");
-        credentials.put("tokenSecret", "mock_tokenSecret");
+    public void setUp() {
+        prepareBatchMockResponse();
+        prepareStreamingMockResponse();
     }
 
-    /*
-        The expected type of response is in the form like below:
-        HTTP/1.1 200 OK
-        Content-Type: text/plain
-        Transfer-Encoding: chunked
+    @After
+    public void tearDown() {
+        System.clearProperty("twitter4j.restBaseURL");
+    }
 
-        7\r\n
-        Mozilla\r\n
-        9\r\n
-        Developer\r\n
-        7\r\n
-        Network\r\n
-        0\r\n
-        \r\n
-    */
     @Test
     public void streamApiMockTest() {
-        String responseText = new Scanner(Objects.requireNonNull(
-                Thread.currentThread()
-                      .getContextClassLoader()
-                      .getResourceAsStream("stream-response.json")), "UTF-8").useDelimiter("\\A").next();
 
-        StringBuilder responseBuilder = new StringBuilder();
-        for (int i = 0; i < 100; i++) {
-            responseBuilder.append(responseText.length());
-            responseBuilder.append("\r\n");
-            responseBuilder.append(responseText);
-            responseBuilder.append("\r\n");
-        }
-        responseBuilder.append("\r\n");
-        String response = responseBuilder.toString();
-        stubMockResponse(response);
-
-        Pipeline pipeline = Pipeline.create();
         List<String> terms = new ArrayList<>(Arrays.asList("San Mateo", "Brno", "London", "Istanbul"));
-
-        final StreamSource<String> twitterTestStream = TwitterSources.timestampedStream(credentials,
+        final StreamSource<String> twitterTestStream = TwitterSources.timestampedStream(getCredentials(),
                 "http://" + server.getHostName() + ":" + server.getPort(),
                 () -> new StatusesFilterEndpoint().trackTerms(terms));
 
+        Pipeline pipeline = Pipeline.create();
         StreamStage<String> tweets = pipeline
                 .readFrom(twitterTestStream)
                 .withNativeTimestamps(0)
@@ -114,7 +81,8 @@ public class TwitterSourceMockTest extends JetTestSupport {
         tweets.writeTo(AssertionSinks.assertCollectedEventually(10,
                 list -> assertGreaterOrEquals("Emits at least 100 tweets in 1 min.",
                         list.size(), 100)));
-        Job job = jet.newJob(pipeline);
+        Job job = createJetMember().newJob(pipeline);
+
         sleepAtLeastSeconds(5);
 
         try {
@@ -129,22 +97,13 @@ public class TwitterSourceMockTest extends JetTestSupport {
 
     @Test
     public void testBatchMock() {
-        System.setProperty("twitter4j.restBaseURL", "http://" + server.getHostName() + ":" + server.getPort() + "/");
-        String responseText1 = new Scanner(Objects.requireNonNull(
-                Thread.currentThread()
-                      .getContextClassLoader()
-                      .getResourceAsStream("search-response1.json")), "UTF-8").useDelimiter("\\A").next();
-        String responseText2 = new Scanner(Objects.requireNonNull(
-                Thread.currentThread()
-                      .getContextClassLoader()
-                      .getResourceAsStream("search-response2.json")), "UTF-8").useDelimiter("\\A").next();
-        stubMockResponse(responseText1);
-        stubMockResponse(responseText2);
+        String baseUrl = "http://" + server.getHostName() + ":" + server.getPort() + "/";
+        System.setProperty("twitter4j.restBaseURL", baseUrl);
 
         Pipeline pipeline = Pipeline.create();
         String query = "Jet flies";
         BatchSource<Status> twitterSearch = TwitterSources.search(
-                credentials, query);
+                getCredentials(), query);
         BatchStage<String> tweets = pipeline
                 .readFrom(twitterSearch)
                 .map(status -> "@" + status.getUser().getName() + " - " + status.getText());
@@ -152,16 +111,10 @@ public class TwitterSourceMockTest extends JetTestSupport {
         tweets.writeTo(AssertionSinks.assertCollectedEventually(10,
                 list -> assertGreaterOrEquals("Emits at least 30 tweets in 10 secs.",
                         list.size(), 30)));
-        Job job = jet.newJob(pipeline);
+        Job job = createJetMember().newJob(pipeline);
+
         sleepAtLeastSeconds(2);
-        try {
-            job.join();
-            fail("Job should have completed with an AssertionCompletedException, but completed normally");
-        } catch (CompletionException e) {
-            String errorMsg = e.getCause().getMessage();
-            assertTrue("Job was expected to complete with AssertionCompletedException, but completed with: "
-                    + e.getCause(), errorMsg.contains(AssertionCompletedException.class.getName()));
-        }
+
         try {
             job.join();
             fail("Job should have completed with an AssertionCompletedException, but completed normally");
@@ -171,12 +124,63 @@ public class TwitterSourceMockTest extends JetTestSupport {
                     + e.getCause(), errorMsg.contains(AssertionCompletedException.class.getName()));
         }
     }
+
+    /*
+       The expected type of response is in the form like below:
+       HTTP/1.1 200 OK
+       Content-Type: text/plain
+       Transfer-Encoding: chunked
+
+       7\r\n
+       Mozilla\r\n
+       9\r\n
+       Developer\r\n
+       7\r\n
+       Network\r\n
+       0\r\n
+       \r\n
+    */
+    private void prepareStreamingMockResponse() {
+        String responseText = loadResponseFromFile("stream-response.json");
+        StringBuilder responseBuilder = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            responseBuilder.append(responseText.length());
+            responseBuilder.append("\r\n");
+            responseBuilder.append(responseText);
+            responseBuilder.append("\r\n");
+        }
+        responseBuilder.append("\r\n");
+        String response = responseBuilder.toString();
+        stubMockResponse(response);
+    }
+
+    private void prepareBatchMockResponse() {
+        stubMockResponse(loadResponseFromFile("search-response1.json"));
+        stubMockResponse(loadResponseFromFile("search-response2.json"));
+    }
+
+    private String loadResponseFromFile(String s) {
+        return new Scanner(Objects.requireNonNull(
+                Thread.currentThread()
+                      .getContextClassLoader()
+                      .getResourceAsStream(s)), "UTF-8").useDelimiter("\\A").next();
+    }
+
     private void stubMockResponse(String response) {
         MockResponse mockResponse = new MockResponse()
                 .addHeader("Content-Type", "application/json; charset=utf-8")
                 .setResponseCode(200)
                 .setChunkedBody(response, 4096);
         server.enqueue(mockResponse);
+    }
+
+    private Properties getCredentials() {
+        Properties credentials = new Properties();
+        credentials.put("consumerKey", "mock_consumerKey");
+        credentials.put("consumerSecret", "mock_consumerSecret");
+        credentials.put("token", "mock_token");
+        credentials.put("tokenSecret", "mock_tokenSecret");
+        return  credentials;
     }
 
 }
