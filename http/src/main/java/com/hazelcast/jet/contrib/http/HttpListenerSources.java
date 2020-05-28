89de0e5144d60d3816a8d92f9ad4684fac30b5d5
Emin Demirci <emin@hazelcast.com>
2020-05-28 13:24:25 +0300

    Adapt upstream changes regarding JSON parsing

--- http/src/main/java/com/hazelcast/jet/contrib/http/HttpListenerSources.java
+++ http/src/main/java/com/hazelcast/jet/contrib/http/HttpListenerSources.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2019, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,15 +16,15 @@
 
 package com.hazelcast.jet.contrib.http;
 
-import com.hazelcast.internal.json.JsonValue;
+import com.hazelcast.cluster.Address;
+import com.hazelcast.function.FunctionEx;
 import com.hazelcast.jet.JetInstance;
-import com.hazelcast.jet.contrib.http.marshalling.MarshallingStrategy;
+import com.hazelcast.jet.json.JsonUtil;
 import com.hazelcast.jet.pipeline.SourceBuilder;
 import com.hazelcast.jet.pipeline.SourceBuilder.SourceBuffer;
 import com.hazelcast.jet.pipeline.StreamSource;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
-import com.hazelcast.nio.Address;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 
@@ -32,21 +32,49 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.net.ssl.SSLContext;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.function.Supplier;
 
-import static com.hazelcast.util.Preconditions.checkNotNull;
-import static com.hazelcast.util.Preconditions.checkPositive;
+import static com.hazelcast.internal.util.Preconditions.checkNotNull;
+import static com.hazelcast.internal.util.Preconditions.checkPositive;
+
 
 /**
- * Contains factory methods for creating HTTP listener sources which listens for HTTP
- * requests which contains JSON payloads. The payload will be parsed as {@link JsonValue}
- * and emitted to downstream.
+ * Contains factory methods for creating HTTP(S) listener sources which listens for HTTP(S)
+ * requests which contains JSON payloads. The payloads will be either parsed as provided
+ * class typed or {@link Map} objects and emitted to the downstream.
  */
 public final class HttpListenerSources {
 
     private HttpListenerSources() {
+    }
+
+    /**
+     * Creates a source that listens for HTTP requests which contains JSON payload.
+     * Payload will be mapped to an object of the provided class type.
+     *
+     * @param portOffset The offset for HTTP listener port to bind from the member
+     *                   port. For example; if Hazelcast Jet member runs on the port
+     *                   5701 and {@code portOffset} is set to 100, the HTTP listeners
+     *                   will listen for connections on port 5801 on the same host
+     *                   address with the member.
+     * @param type       Class type for the objects to be emitted. Received JSON
+     *                   payloads will be mapped to objects of provided type.
+     * @return mapped object from the payload of the HTTP request
+     */
+    @Nonnull
+    public static <T> StreamSource<T> httpListener(@Nonnull int portOffset, @Nonnull Class<T> type) {
+        checkPositive(portOffset, "portOffset cannot be negative");
+        checkNotNull(type, "type cannot be null");
+
+        return SourceBuilder.stream("http-listener(base port +" + portOffset + ")", ctx ->
+                new HttpListenerSourceContext<>(ctx.jetInstance(), portOffset, type))
+                .<T>fillBufferFn(HttpListenerSourceContext::fillBuffer)
+                .destroyFn(HttpListenerSourceContext::close)
+                .distributed(1)
+                .build();
     }
 
     /**
@@ -57,18 +85,50 @@ public final class HttpListenerSources {
      *                   5701 and {@code portOffset} is set to 100, the HTTP listeners
      *                   will listen for connections on port 5801 on the same host
      *                   address with the member.
-     * @return {@link JsonValue}, parsed JSON payload on HTTP request
+     * @return {@link Map} object which contains parsed JSON fields from the HTTP payload
      */
     @Nonnull
-    public static <T> StreamSource<T> httpListener(@Nonnull int portOffset, @Nonnull MarshallingStrategy<T> marshallingStrategy) {
+    public static StreamSource<Map<String, Object>> httpListener(@Nonnull int portOffset) {
         checkPositive(portOffset, "portOffset cannot be negative");
 
         return SourceBuilder.stream("http-listener(base port +" + portOffset + ")", ctx ->
-                new HttpListenerSourceContext<>(ctx.jetInstance(), portOffset, marshallingStrategy))
-                            .<T>fillBufferFn(HttpListenerSourceContext::fillBuffer)
-                            .destroyFn(HttpListenerSourceContext::close)
-                            .distributed(1)
-                            .build();
+                new HttpListenerSourceContext<Map<String, Object>>(ctx.jetInstance(), portOffset, null))
+                .<Map<String, Object>>fillBufferFn(HttpListenerSourceContext::fillBuffer)
+                .destroyFn(HttpListenerSourceContext::close)
+                .distributed(1)
+                .build();
+    }
+
+
+    /**
+     * Creates a source that listens for HTTPS requests which contains JSON payload.
+     * Payload will be mapped to an object of the provided class type.
+     *
+     * @param portOffset         The offset for HTTPS listener port to bind from the member
+     *                           port. For example; if Hazelcast Jet member runs on the port
+     *                           5701 and {@code portOffset} is set to 100, the HTTPS server
+     *                           will listen for connections on port 5801 on the same host
+     *                           address with the member.
+     * @param sslContextSupplier the function to create {@link SSLContext} which
+     *                           used to initialize underlying HTTPS listener for
+     *                           secure connections.
+     * @param type               Class type for the objects to be emitted. Received JSON
+     *                           payloads will be mapped to objects of provided type.
+     * @return mapped object from the payload of the HTTPS request
+     */
+    @Nonnull
+    public static <T> StreamSource<T> httpsListener(@Nonnull int portOffset,
+                                                    @Nonnull Supplier<SSLContext> sslContextSupplier,
+                                                    @Nonnull Class<T> type) {
+        checkPositive(portOffset, "portOffset cannot be negative");
+        checkNotNull(type, "type cannot be null");
+
+        return SourceBuilder.stream("https-listener(base port + " + portOffset + ")", ctx ->
+                new HttpListenerSourceContext<>(ctx.jetInstance(), portOffset, type, sslContextSupplier.get()))
+                .<T>fillBufferFn(HttpListenerSourceContext::fillBuffer)
+                .destroyFn(HttpListenerSourceContext::close)
+                .distributed(1)
+                .build();
     }
 
     /**
@@ -82,21 +142,21 @@ public final class HttpListenerSources {
      * @param sslContextSupplier the function to create {@link SSLContext} which
      *                           used to initialize underlying HTTPS listener for
      *                           secure connections.
-     * @return {@link JsonValue} parsed JSON payload on HTTPS request
+     * @return {@link Map} object which contains parsed JSON fields from the HTTPS payload
      */
     @Nonnull
-    public static <T> StreamSource<T> httpsListener(@Nonnull int portOffset,
-                                                    @Nonnull Supplier<SSLContext> sslContextSupplier,
-                                                    @Nonnull MarshallingStrategy<T> marshallingStrategy) {
+    public static StreamSource<Map<String, Object>> httpsListener(@Nonnull int portOffset,
+                                                                  @Nonnull Supplier<SSLContext> sslContextSupplier) {
         checkPositive(portOffset, "portOffset cannot be negative");
         checkNotNull(sslContextSupplier, "sslContextSupplier cannot be null");
 
         return SourceBuilder.stream("https-listener(base port + " + portOffset + ")", ctx ->
-                new HttpListenerSourceContext<>(ctx.jetInstance(), portOffset, marshallingStrategy, sslContextSupplier.get()))
-                            .<T>fillBufferFn(HttpListenerSourceContext::fillBuffer)
-                            .destroyFn(HttpListenerSourceContext::close)
-                            .distributed(1)
-                            .build();
+                new HttpListenerSourceContext<Map<String, Object>>(ctx.jetInstance(), portOffset, null,
+                        sslContextSupplier.get()))
+                .<Map<String, Object>>fillBufferFn(HttpListenerSourceContext::fillBuffer)
+                .destroyFn(HttpListenerSourceContext::close)
+                .distributed(1)
+                .build();
     }
 
     private static class HttpListenerSourceContext<T> {
@@ -107,35 +167,40 @@ public final class HttpListenerSources {
         private final BlockingQueue<T> queue = new ArrayBlockingQueue<>(1000);
         private final ArrayList<T> buffer = new ArrayList<>(MAX_FILL_ELEMENTS);
         private final Undertow undertow;
-        private MarshallingStrategy<T> marshallingStrategy;
+        private FunctionEx<String, T> mappingFunction;
         private final HttpHandler handler = exchange ->
                 exchange
                         .getRequestReceiver()
                         .receiveFullString((e, message) -> {
-                            queue.offer(marshallingStrategy.getObject(message));
+                            queue.offer(mappingFunction.apply(message));
                             e.endExchange();
                         });
 
-        HttpListenerSourceContext(JetInstance jet, int portOffset, MarshallingStrategy<T> marshallingStrategy) {
-            this(jet, portOffset, marshallingStrategy, null);
+        HttpListenerSourceContext(JetInstance jet, int portOffset, @Nullable Class<T> type) {
+            this(jet, portOffset, type, null);
         }
 
-        HttpListenerSourceContext(JetInstance jet, int portOffset, MarshallingStrategy<T> marshallingStrategy, @Nullable SSLContext sslContext) {
-            this.marshallingStrategy = marshallingStrategy;
+        HttpListenerSourceContext(JetInstance jet, int portOffset, @Nullable Class<T> type,
+                                  @Nullable SSLContext sslContext) {
+            if (type == null) {
+                mappingFunction = message -> (T) JsonUtil.mapFrom(message);
+            } else {
+                mappingFunction = message -> JsonUtil.beanFrom(message, type);
+            }
             Address localAddress = jet.getHazelcastInstance().getCluster().getLocalMember().getAddress();
             String host = localAddress.getHost();
             int port = localAddress.getPort() + portOffset;
             if (sslContext != null) {
                 undertow = Undertow.builder()
-                                   .addHttpsListener(port, host, sslContext)
-                                   .setHandler(handler)
-                                   .build();
+                        .addHttpsListener(port, host, sslContext)
+                        .setHandler(handler)
+                        .build();
                 LOGGER.info("Starting to listen HTTPS messages on https://" + host + ":" + port);
             } else {
                 undertow = Undertow.builder()
-                                   .addHttpListener(port, host)
-                                   .setHandler(handler)
-                                   .build();
+                        .addHttpListener(port, host)
+                        .setHandler(handler)
+                        .build();
                 LOGGER.info("Starting to listen HTTP messages on http://" + host + ":" + port);
             }
             undertow.start();
--- http/src/test/java/com/hazelcast/jet/contrib/http/HttpListenerSourceTest.java
+++ http/src/test/java/com/hazelcast/jet/contrib/http/HttpListenerSourceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2019, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,17 +16,15 @@
 
 package com.hazelcast.jet.contrib.http;
 
-import com.hazelcast.internal.json.JsonObject;
-import com.hazelcast.internal.json.JsonValue;
+import com.hazelcast.cluster.Address;
+import com.hazelcast.function.SupplierEx;
 import com.hazelcast.jet.JetInstance;
 import com.hazelcast.jet.Job;
-import com.hazelcast.jet.contrib.http.marshalling.impl.HazelcastInternalJsonMarshallingStrategy;
 import com.hazelcast.jet.core.JetTestSupport;
 import com.hazelcast.jet.core.JobStatus;
-import com.hazelcast.jet.function.SupplierEx;
+import com.hazelcast.jet.json.JsonUtil;
 import com.hazelcast.jet.pipeline.Pipeline;
 import com.hazelcast.jet.pipeline.test.AssertionCompletedException;
-import com.hazelcast.nio.Address;
 import com.hazelcast.nio.ssl.TestKeyStoreUtil;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -46,7 +44,9 @@ import javax.net.ssl.TrustManagerFactory;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.Serializable;
 import java.security.KeyStore;
+import java.util.Objects;
 
 import static com.hazelcast.jet.core.TestUtil.executeAndPeel;
 import static com.hazelcast.jet.pipeline.test.AssertionSinks.assertCollectedEventually;
@@ -59,32 +59,110 @@ public class HttpListenerSourceTest extends JetTestSupport {
     public ExpectedException expectedException = ExpectedException.none();
 
     @Test
-    public void testHttpIngestion() throws Throwable {
+    public void testHttpIngestion_with_objectMapping() throws Throwable {
         JetInstance jet = createJetMember();
         JetInstance jet2 = createJetMember();
 
-        Address localAddress = jet2.getHazelcastInstance().getCluster().getLocalMember().getAddress();
         int portOffset = 100;
-        int port = localAddress.getPort() + portOffset;
-        String httpEndpoint = "http://" + localAddress.getHost() + ":" + port;
+        String httpEndpoint1 = getEndpointAddress(jet, portOffset, false);
+        String httpEndpoint2 = getEndpointAddress(jet2, portOffset, false);
 
         Pipeline p = Pipeline.create();
 
-        p.drawFrom(HttpListenerSources.httpListener(portOffset, new HazelcastInternalJsonMarshallingStrategy()))
-         .withoutTimestamps()
-         .filter(object -> object.asObject().get("id").asInt() >= 80)
-         .drainTo(assertCollectedEventually(30, list -> assertEquals(20, list.size())));
+        p.readFrom(HttpListenerSources.httpListener(portOffset, User.class))
+                .withoutTimestamps()
+                .filter(user -> user.id >= 80)
+                .writeTo(assertCollectedEventually(30, list -> assertEquals(20, list.size())));
 
         Job job = jet.newJob(p);
         assertJobStatusEventually(job, JobStatus.RUNNING);
         sleepAtLeastSeconds(3);
 
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        postUsers(httpClient, 100, httpEndpoint);
+        postUsers(httpClient, 100, httpEndpoint1, httpEndpoint2);
 
         expectedException.expectCause(instanceOf(AssertionCompletedException.class));
         executeAndPeel(job);
     }
+
+    @Test
+    public void testHttpIngestion() throws Throwable {
+        JetInstance jet = createJetMember();
+        JetInstance jet2 = createJetMember();
+
+        int portOffset = 100;
+        String httpEndpoint1 = getEndpointAddress(jet, portOffset, false);
+        String httpEndpoint2 = getEndpointAddress(jet2, portOffset, false);
+
+        Pipeline p = Pipeline.create();
+
+        p.readFrom(HttpListenerSources.httpListener(portOffset))
+                .withoutTimestamps()
+                .filter(map -> (int) map.get("id") >= 80)
+                .writeTo(assertCollectedEventually(30, list -> assertEquals(20, list.size())));
+
+        Job job = jet.newJob(p);
+        assertJobStatusEventually(job, JobStatus.RUNNING);
+        sleepAtLeastSeconds(3);
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        postUsers(httpClient, 100, httpEndpoint1, httpEndpoint2);
+
+        expectedException.expectCause(instanceOf(AssertionCompletedException.class));
+        executeAndPeel(job);
+    }
+
+    @Test
+    public void testHttpsIngestion_with_objectMapping() throws Throwable {
+        SupplierEx<SSLContext> contextSupplier = () -> {
+            SSLContext context = SSLContext.getInstance("TLS");
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            KeyStore ks = KeyStore.getInstance("JKS");
+            char[] password = "123456".toCharArray();
+            File tempFile = TestKeyStoreUtil.createTempFile(TestKeyStoreUtil.keyStore);
+            ks.load(new FileInputStream(tempFile), password);
+            kmf.init(ks, password);
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            KeyStore ts = KeyStore.getInstance("JKS");
+            File tsfile = TestKeyStoreUtil.createTempFile(TestKeyStoreUtil.trustStore);
+            ts.load(new FileInputStream(tsfile), password);
+            tmf.init(ts);
+
+            context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            return context;
+        };
+
+        JetInstance jet = createJetMember();
+        JetInstance jet2 = createJetMember();
+
+        int portOffset = 100;
+        String httpEndpoint1 = getEndpointAddress(jet, portOffset, true);
+        String httpEndpoint2 = getEndpointAddress(jet2, portOffset, true);
+
+        Pipeline p = Pipeline.create();
+
+        p.readFrom(HttpListenerSources.httpsListener(portOffset, contextSupplier, User.class))
+                .withoutTimestamps()
+                .filter(user -> user.id >= 80)
+                .writeTo(assertCollectedEventually(30, list -> assertEquals(20, list.size())));
+
+        Job job = jet.newJob(p);
+
+        assertJobStatusEventually(job, JobStatus.RUNNING);
+        sleepAtLeastSeconds(3);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLContext(contextSupplier.get())
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .setRetryHandler(new DefaultHttpRequestRetryHandler(10, true))
+                .build();
+        postUsers(httpClient, 100, httpEndpoint1, httpEndpoint2);
+
+        expectedException.expectCause(instanceOf(AssertionCompletedException.class));
+        executeAndPeel(job);
+    }
+
 
     @Test
     public void testHttpsIngestion() throws Throwable {
@@ -110,18 +188,16 @@ public class HttpListenerSourceTest extends JetTestSupport {
         JetInstance jet = createJetMember();
         JetInstance jet2 = createJetMember();
 
-        Address localAddress = jet2.getHazelcastInstance().getCluster().getLocalMember().getAddress();
         int portOffset = 100;
-        int port = localAddress.getPort() + portOffset;
-        String httpsEndpoint = "https://" + localAddress.getHost() + ":" + port;
+        String httpEndpoint1 = getEndpointAddress(jet, portOffset, true);
+        String httpEndpoint2 = getEndpointAddress(jet2, portOffset, true);
 
         Pipeline p = Pipeline.create();
 
-        p.drawFrom(HttpListenerSources.httpsListener(portOffset, contextSupplier, new HazelcastInternalJsonMarshallingStrategy()))
-         .withoutTimestamps()
-         .map(JsonValue::asObject)
-         .filter(object -> object.get("id").asInt() >= 80)
-         .drainTo(assertCollectedEventually(30, list -> assertEquals(20, list.size())));
+        p.readFrom(HttpListenerSources.httpsListener(portOffset, contextSupplier))
+                .withoutTimestamps()
+                .filter(map -> (int) map.get("id") >= 80)
+                .writeTo(assertCollectedEventually(30, list -> assertEquals(20, list.size())));
 
         Job job = jet.newJob(p);
 
@@ -129,29 +205,90 @@ public class HttpListenerSourceTest extends JetTestSupport {
         sleepAtLeastSeconds(3);
 
         CloseableHttpClient httpClient = HttpClients.custom()
-                                                    .setSSLContext(contextSupplier.get())
-                                                    .setSSLHostnameVerifier(new NoopHostnameVerifier())
-                                                    .setRetryHandler(new DefaultHttpRequestRetryHandler(10, true))
-                                                    .build();
-        postUsers(httpClient, 100, httpsEndpoint);
+                .setSSLContext(contextSupplier.get())
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .setRetryHandler(new DefaultHttpRequestRetryHandler(10, true))
+                .build();
+        postUsers(httpClient, 100, httpEndpoint1, httpEndpoint2);
 
         expectedException.expectCause(instanceOf(AssertionCompletedException.class));
         executeAndPeel(job);
     }
 
-    private void postUsers(CloseableHttpClient httpClient, int count, String uri) throws IOException {
+
+    private String getEndpointAddress(JetInstance jet, int portOffset, boolean ssl) {
+        Address localAddress = jet.getHazelcastInstance().getCluster().getLocalMember().getAddress();
+        int port = localAddress.getPort() + portOffset;
+        String hostPort = localAddress.getHost() + ":" + port;
+        return ssl ? "https://" + hostPort : "http://" + hostPort;
+    }
+
+    private void postUsers(CloseableHttpClient httpClient, int count, String uri1, String uri2) throws IOException {
         for (int i = 0; i < count; i++) {
-            JsonObject object = new JsonObject();
-            object.add("id", i);
-            object.add("name", "name" + i);
+            User user = new User(i, "name" + i);
+            String jsonString = JsonUtil.toJson(user);
             StringEntity requestEntity = new StringEntity(
-                    object.toString(),
+                    jsonString,
                     ContentType.APPLICATION_JSON);
-            HttpPost post = new HttpPost(uri);
+            HttpPost post = i % 2 == 0 ? new HttpPost(uri1) : new HttpPost(uri2);
             post.setEntity(requestEntity);
             CloseableHttpResponse response = httpClient.execute(post);
             response.getEntity().getContent().close();
         }
     }
 
+    public static class User implements Serializable {
+        private int id;
+        private String name;
+
+        public User() {
+        }
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return Objects.equals(id, user.id) &&
+                    Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "id='" + id + '\'' +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
 }
