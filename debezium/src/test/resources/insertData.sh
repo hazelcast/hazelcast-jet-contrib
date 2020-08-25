f86356c27d030c49e72b32671cfddf1d489544f3
Bartok Jozsef <jozsi@hazelcast.com>
2020-08-25 14:28:37 +0300

    Remove debezium module

--- debezium/src/test/resources/insertData.sh
+++
@@ -1,6 +0,0 @@
-mongo -u debezium -p dbz --authenticationDatabase admin localhost:27017/inventory <<-EOF
-    use inventory;
-    db.customers.insert([
-        { _id : NumberLong("1005"), first_name : 'Jason', last_name : 'Bourne', email : 'jason@bourne.org' }
-    ]);
-EOF
