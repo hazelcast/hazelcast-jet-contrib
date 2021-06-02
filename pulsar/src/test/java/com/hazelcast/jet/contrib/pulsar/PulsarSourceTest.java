9c471743cd18beb67692d997e0e0965cd73a3a9d
Ufuk Yılmaz <ufuk.yilmaz@hazelcast.com>
2021-06-02 15:44:38 +0300

    Add distributed edge to the pipeline
    After the light job adaptation, for using this assertJobRunEventually,
    we need to make the job runs on all members. So, I added a distributed
    edge to the pipeline.

--- pulsar/src/test/java/com/hazelcast/jet/contrib/pulsar/PulsarSourceTest.java
+++ pulsar/src/test/java/com/hazelcast/jet/contrib/pulsar/PulsarSourceTest.java
@@ -17,6 +17,7 @@
 package com.hazelcast.jet.contrib.pulsar;
 
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.jet.JetInstance;
 import com.hazelcast.jet.Job;
 import com.hazelcast.jet.config.JobConfig;
 import com.hazelcast.jet.config.ProcessingGuarantee;
@@ -144,6 +145,7 @@ public class PulsarSourceTest extends PulsarTestSupport {
         Pipeline pipeline = Pipeline.create();
         pipeline.readFrom(pulsarReaderSrc)
                 .withoutTimestamps()
+                .rebalance()
                 .writeTo(Sinks.list("test-list"));
 
         JobConfig jobConfig = new JobConfig();
@@ -176,16 +178,9 @@ public class PulsarSourceTest extends PulsarTestSupport {
             });
             // Bring down one member. Job should restart and drain additional items (and maybe
             // some of the previous duplicately).
+            Long lastExecutionId = assertJobRunningEventually((JetInstance) instances[0].getJet(), job, null);
             instances[1].getLifecycleService().terminate();
-            sleepMillis(2_000);
-            // Instead of sleep we used to synchronize the job restart with
-            // assertJobRunningEventually. But, this assertion is running a bit
-            // flaky now after light job changes. So, we switched to using
-            // sleep. The old version was as follows:
-            // Long lastExecutionId = assertJobRunningEventually((JetInstance) instances[0].getJet(), job, null);
-            // instances[1].getLifecycleService().terminate();
-            // assertJobRunningEventually((JetInstance) instances[0].getJet(), job, lastExecutionId);
-            // TODO: Fix and reuse assertJobRunningEventually
+            assertJobRunningEventually((JetInstance) instances[0].getJet(), job, lastExecutionId);
 
             produceMessages("after-restart", topicName, 2 * ITEM_COUNT);
 
